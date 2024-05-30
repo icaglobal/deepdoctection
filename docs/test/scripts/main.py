cac3a5474182af7128e3842b9c7db3894130b317
peterhvoth <55630232+peterhvoth@users.noreply.github.com>
2024-05-30 09:52:13 -0400

    add xfa/acroform test code

---
+++ docs/test/scripts/main.py
@@ -0,0 +1,15 @@
+import deepdoctection as dd
+from deepdoctection.pipe.form import FormHandler
+from pypdf import PdfReader
+
+path = '../pdf/acroform.pdf'
+
+#analyzer = dd.get_dd_analyzer()
+#df = analyzer.analyze(path=path)
+#df.reset_state()
+#pdf = analyzer.pdf_reader
+
+pdf = PdfReader(open(path, 'rb'))
+
+af = FormHandler(pdf)
+print(af.result)
