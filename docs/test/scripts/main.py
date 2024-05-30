274ef88d730327a24dfbfaa9fc6ec97a64e5f241
Peter Voth <peter.voth@fda.hhs.gov>
2024-05-30 11:17:04 -0400

    add additional pdf paths

--- docs/test/scripts/main.py
+++ docs/test/scripts/main.py
@@ -2,6 +2,8 @@ import deepdoctection as dd
 from deepdoctection.pipe.form import FormHandler
 from pypdf import PdfReader
 
+path = '../pdf/normal.pdf'
+path = '../pdf/estar.pdf'
 path = '../pdf/acroform.pdf'
 
 #analyzer = dd.get_dd_analyzer()
